049897cc8dacb50753b94247f2ec71ac38eeab61
Zareen20 <87030781+Zareen20@users.noreply.github.com>
2021-08-26 02:45:00 +0600

    Add files via upload

---
+++ Controller/Admin/home.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+if (isset($_SESSION["username"])) {
+    $username = $_SESSION["username"];
+    session_write_close();
+} else {
+    // since the username is not set in session, the user is not-logged-in
+    // he is trying to access this page unauthorized
+    // so let's clear all session variables and redirect him to index
+    session_unset();
+    session_write_close();
+    $url = "./index.php";
+    header("Location: $url");
+}
+
+?>
+<HTML>
+<HEAD>
+<TITLE>Welcome</TITLE>
+<link href="assets/css/phppot-style.css" type="text/css"
+	rel="stylesheet" />
+<link href="assets/css/user-registration.css" type="text/css"
+	rel="stylesheet" />
+</HEAD>
+<BODY>
+	<div class="phppot-container">
+		<div class="page-header">
+			<span class="login-signup"><a href="logout.php">Logout</a></span>
+		</div>
+		<div class="page-content">Welcome <?php echo $username;?></div>
+	</div>
+<p align="center">
+    <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user-registration";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, name, contact, product FROM tbl_order";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "Name: " . $row["name"]. "-Mobile:  " . $row["contact"]. "-Product Details: " . $row["product"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+</p>
+</BODY>
+</HTML>
